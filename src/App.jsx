133896b0e79adf8b6d904c71aec4ae5ae10ff631
Johan E Cano Ruiz <johan.memincho@gmail.com>
2024-07-25 09:28:07 -0500

    Styles OK

---
+++ src/App.jsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+import { Person } from './components/Person'
+
+function App() {
+
+  const [persons, setPersons] = useState([
+    {
+      id: 1,
+      name: "Johan",
+      role: "Frontend Developer",
+      img: "https://bootdey.com/img/Content/avatar/avatar1.png"
+    },
+    {
+      id: 2,
+      name: "Eduardo",
+      role: "Backend Developer",
+      img: "https://bootdey.com/img/Content/avatar/avatar6.png"
+    },
+    {
+      id: 3,
+      name: "Cano",
+      role: "FullStack Developer",
+      img: "https://bootdey.com/img/Content/avatar/avatar2.png"
+    }
+  ]);
+
+  return (
+    <div className='container'>
+      <div className='row'>
+        {persons.map((person) => {
+          return (
+          <Person
+            key={person.id}
+            name={person.name}
+            img={person.img}
+            role={person.role}
+          />
+          );
+        })}
+      </div>
+    </div>
+  )
+}
+
+export default App
